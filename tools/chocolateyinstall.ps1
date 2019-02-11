2578fd0f68ca56ba7c466038be3be9a5fd2fddb1
Eduardo Argollo <eargollo@gmail.com>
2019-02-11 10:57:48 -0800

    Updated to version 2.7.0-beta

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Xyrio/RSSOwlnix/releases/download/2.6.1/RSSOwlnix-2-6-1-win32.win32.x86.zip' # download url, HTTPS preferred
-$url64      = 'https://github.com/Xyrio/RSSOwlnix/releases/download/2.6.1/RSSOwlnix-2-6-1-win32.win32.x86_64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://github.com/Xyrio/RSSOwlnix/releases/download/2.7.0/RSSOwlnix-2-7-0-win32.win32.x86.zip' # download url, HTTPS preferred
+$url64      = 'https://github.com/Xyrio/RSSOwlnix/releases/download/2.7.0/RSSOwlnix-2-7-0-win32.win32.x86_64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'RSSOwlnix*'
 
-  checksum      = '3E28B9A5AA67FC556905795B505955ED98503D4A84FD4971E4C031F880AF13CF'
+  checksum      = 'B32F3874D448F3D1EC758649DDFAD284BF6C9E9CDD554D45EA0CAAB1D4F9FED6'
   checksumType  = 'sha256' 
-  checksum64    = '61FABCBEE56645558E176D0426C46DFA1D1FF400630A34C6737F94ACFBCEF876'
+  checksum64    = 'EC68294C7ED8A753155E71B30411C6128051AE47DB3A61F7B74B94C66E1DDE91'
   checksumType64= 'sha256' 
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
